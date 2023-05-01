f33d6cd17ce038868590baa6b7226be5d8786002
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2023-05-02 00:30:14 +0300

    Update next.config.js
    disable eslint in prod

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     appDir: true,
     mdxRs: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 const withMDX = require('@next/mdx')()
